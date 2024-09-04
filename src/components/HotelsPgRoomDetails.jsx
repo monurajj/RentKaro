c23bc8b9b0a758ac0ebbf1f15806a191f25d042c
Monu Rajj <monu2feb2004.com>
2024-09-05 02:11:34 +0530

    changes button ui

--- src/components/HotelsPgRoomDetails.jsx
+++ src/components/HotelsPgRoomDetails.jsx
@@ -44,12 +44,11 @@ const HotelsPgRoomDetails = () => {
                 </p> */}
 
                 {/* Buttons */}
-                {/* Buttons */}
                 <div className="flex space-x-4 mt-4">
-                  <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  <button className="btn-1 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
                     View Details
                   </button>
-                  <button className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+                  <button className="btn-11 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
                     Book Now
                   </button>
                 </div>
